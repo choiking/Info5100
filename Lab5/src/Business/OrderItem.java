0a3b35c852dacf8b4df86c5fa0935aa97a1cfcdf
samsamtu <tu.n@husky.neu.edu>
2018-02-14 11:45:03 -0500

    added lab5

---
+++ Lab5/src/Business/OrderItem.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+/**
+ *
+ * @author boyabi
+ */
+public class OrderItem {
+  private int quatity;
+  private double salesPrice;
+  private Product product;
+
+    public int getQuatity() {
+        return quatity;
+    }
+
+    public void setQuatity(int quatity) {
+        this.quatity = quatity;
+    }
+
+    public double getSalesPrice() {
+        return salesPrice;
+    }
+
+    public void setSalesPrice(double salesPrice) {
+        this.salesPrice = salesPrice;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+  
+    @Override
+  public String toString(){
+      return product.getProdName();
+  }
+     
+}
